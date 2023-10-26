2714bc0138bbccd70041312e78f4bf890409c685
rotset <magalyrotset@gmail.com>
2023-10-26 15:36:00 -0600

    commit registro

--- JS/personal_funciones.js
+++ JS/personal_funciones.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+
     var opcion;
      var dataTablePersonal = $('#personales').DataTable({
        select:true,
@@ -36,6 +38,8 @@ $(document).ready(function() {
      }
      });
 // ******************************* Agregar empleado / personal  ***********************
+//var fordata = new FormData(document.getElementById("agregarPersonal"));
+//console.log(fordata);
 
      $(document).on('submit', '#agregarPersonal', function(e) {
       e.preventDefault();
@@ -44,6 +48,7 @@ $(document).ready(function() {
       var per_direccion    = $('#per_direccion').val();
       var per_colonia      = $('#per_colonia').val();
       var per_municipio    = $('#per_municipio').val();
+      var per_fechaNac    = $('#per_fechaNac').val();
       var per_telefono     = $('#per_telefono').val();
       var per_emergencia   = $('#per_emergencia').val();
       var per_sexo         = $('#per_sexo').val();
@@ -62,10 +67,13 @@ $(document).ready(function() {
       var per_entradaComida= $('#per_entradaComida').val();
       var per_hraSalida    = $('#per_hraSalida').val();
       var per_correo       = $('#per_correo').val();
+     // var per_foto         = $('#foto').val();
+     // var per_anverso       = $('#per_anverso').val();
+     // var per_reverso       = $('#per_reverso').val();
 
    
      if ( per_nombre === '' || per_apellidos === ''|| per_direccion == '' || per_colonia ==='' || per_municipio =='' || per_telefono == '' || per_emergencia =='' || per_sexo == ''
-          || per_estadocivil == ''|| per_escolaridad == ''|| per_sucursal == ''|| per_puesto == ''|| per_fechaalta == ''|| per_manejaauto == ''|| per_tipo == ''|| per_subtipo == ''
+          || per_estadocivil == ''|| per_escolaridad == ''|| per_sucursal == ''|| per_puesto == ''|| per_fechaalta == ''|| per_fechaalta == ''|| per_manejaauto == ''|| per_tipo == ''|| per_subtipo == ''
           || per_sueldobase  == ''|| per_sueldoimss  == ''|| per_hraEntrada == ''|| per_hraEntrada == ''|| per_salidaComida == ''|| per_entradaComida == ''|| per_hraSalida == ''
           || per_correo == '') {
           Swal.fire({
@@ -74,41 +82,30 @@ $(document).ready(function() {
             text    : 'No ingresaste todos los campos!',
           });
       } else { 
+        var formData = new FormData(document.getElementById("agregarPersonal"));
+        console.log(formData);
         $.ajax({
           url: "../../Modelo/personal_agregar.php",
           type: "post",
-          data: {
-            per_nombre          : per_nombre,     
-            per_apellidos       : per_apellidos,
-            per_direccion       : per_direccion,
-            per_colonia         : per_colonia,
-            per_municipio       : per_municipio,
-            per_telefono        : per_telefono,
-            per_emergencia      : per_emergencia,
-            per_sexo            : per_sexo,
-            per_estadocivil     : per_estadocivil,
-            per_escolaridad     : per_escolaridad,
-            per_sucursal        : per_sucursal,
-            per_puesto          : per_puesto,
-            per_fechaalta       : per_fechaalta,
-            per_manejaauto      : per_manejaauto,
-            per_tipo            : per_tipo,
-            per_subtipo         : per_subtipo,
-            per_sueldobase      : per_sueldobase,
-            per_sueldoimss      : per_sueldoimss,
-            per_hraEntrada      : per_hraEntrada,
-            per_salidaComida    : per_salidaComida,
-            per_entradaComida   : per_entradaComida,
-            per_hraSalida       : per_hraSalida,
-            per_correo          : per_correo,
-          },
+          dataType: "html",
+          data: formData,
+          cache: false,
+          contentType: false,
+          processData: false,
           success: function(data) {
             dataTablePersonal.ajax.reload(null, false);
             var json      = JSON.parse(data);
             var status    = json.status;
             if (status    == 'true') {
               //$('#AgregarCliente').modal('hide');
-                $("#agregarPersonal").trigger("reset");    
+                $("#agregarPersonal").trigger("reset");
+               
+                $("#X").remove();
+                $("#img").remove();
+                $("#X2").remove();
+                $("#img2").remove();
+                $("#X3").remove();
+                $("#img3").remove();     
               Swal.fire({
                 position: 'top-center',
                 icon: 'success',
@@ -116,6 +113,7 @@ $(document).ready(function() {
                 showConfirmButton: false,
                 timer: 1500,    
               })	
+              window.location.href = 'empleados.php';
             } else {
             }
           }
@@ -188,5 +186,150 @@ $(document).on('click', '.deleteBtn', function(event) {
 })
 
 
+
+
+});
+
+$(document).ready(function () {
+	//--------------------- SELECCIONAR FOTO EMPLEADO ---------------------
+	$("#foto").on("change", function () {
+		var uploadFoto = document.getElementById("foto").value;
+    console.log(uploadFoto);
+		var foto = document.getElementById("foto").files;
+		var nav = window.URL || window.webkitURL;
+		var contactAlert = document.getElementById('form_alert');
+
+		if (uploadFoto != '') {
+			var type = foto[0].type;
+			var name = foto[0].name;
+			if (type != 'image/jpeg' && type != 'image/jpg' && type != 'image/png') {
+				contactAlert.innerHTML = '<p class="errorArchivo">El archivo no es válido.</p>';
+				$("#img").remove();
+				$(".delPhoto").addClass('notBlock');
+				$('#foto').val('');
+				return false;
+			} else {
+				contactAlert.innerHTML = '';
+				$("#img").remove();
+				$(".delPhoto").removeClass('notBlock');
+				var objeto_url = nav.createObjectURL(this.files[0]);
+				$(".prevPhoto").append("<img id='img' src=" + objeto_url + ">");
+				$(".upimg label").remove();
+
+			}
+		} else {
+			alert("No selecciono foto");
+			$("#img").remove();
+		}
+	});
+
+	$('.delPhoto').click(function () {
+		$('#foto').val('');
+		$(".delPhoto").addClass('notBlock');
+		$("#img").remove();
+
+	});
+
+
+});
+
+
+
+
+$(document).ready(function () {
+
+  /*IMAGEN ANVERSO*/
+  $("#fotoAnverso").on("change", function () {
+    var uploadFoto = document.getElementById("fotoAnverso").value;
+    console.log(uploadFoto);
+
+    var foto = document.getElementById("fotoAnverso").files;
+    var nav = window.URL || window.webkitURL;
+    var contactAlert = document.getElementById('form_alert2');
+
+    if (uploadFoto != '') {
+      var type = foto[0].type;
+      var name = foto[0].name;
+      if (type != 'image/jpeg' && type != 'image/jpg' && type != 'image/png') {
+        contactAlert.innerHTML = '<p class="errorArchivo">El archivo no es válido.</p>';
+        $("#img2").remove();
+        $(".delPhoto2").addClass('notBlock2');
+        $('#fotoAnverso').val('');
+        return false;
+      } else {
+        contactAlert.innerHTML = '';
+        $("#img2").remove();
+        $(".delPhoto2").removeClass('notBlock2');
+        var objeto_url = nav.createObjectURL(this.files[0]);
+        $(".prevPhoto2").append("<img id='img2' src=" + objeto_url + ">");
+        $(".upimg2 label").remove();
+
+      }
+    } else {
+      alert("No selecciono foto");
+      $("#img2").remove();
+    }
+  });
+
+  $('.delPhoto2').click(function () {
+    $('#fotoAnverso').val('');
+    $(".delPhoto2").addClass('notBlock2');
+    $("#img2").remove();
+
+  });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+
+  /*IMAGEN REVERSO*/
+  $("#fotoReverso").on("change", function () {
+    var uploadFoto = document.getElementById("fotoReverso").value;
+    console.log(uploadFoto);
+
+    var foto = document.getElementById("fotoReverso").files;
+    var nav = window.URL || window.webkitURL;
+    var contactAlert = document.getElementById('form_alert3');
+
+    if (uploadFoto != '') {
+      var type = foto[0].type;
+      var name = foto[0].name;
+      if (type != 'image/jpeg' && type != 'image/jpg' && type != 'image/png') {
+        contactAlert.innerHTML = '<p class="errorArchivo">El archivo no es válido.</p>';
+        $("#img3").remove();
+        $(".delPhoto3").addClass('notBlock3');
+        $('#fotoReverso').val('');
+        return false;
+      } else {
+        contactAlert.innerHTML = '';
+        $("#img3").remove();
+        $(".delPhoto3").removeClass('notBlock3');
+        var objeto_url = nav.createObjectURL(this.files[0]);
+        $(".prevPhoto3").append("<img id='img3' src=" + objeto_url + ">");
+        $(".upimg3 label").remove();
+
+      }
+    } else {
+      alert("No selecciono foto");
+      $("#img3").remove();
+    }
+  });
+
+  $('.delPhoto3').click(function () {
+    $('#fotoReverso').val('');
+    $(".delPhoto3").addClass('notBlock3');
+    $("#img3").remove();
+
+  });
 
 });
--- Vista/recursos-humanos/empleados.php
+++ Vista/recursos-humanos/empleados.php
@@ -16,6 +16,7 @@
   <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <!-- CSS -->
+    <link rel="stylesheet" href="../../Assets/css/style.css">
     <link rel="stylesheet" type="text/css" href="../vendors/styles/core.css" />
     <link rel="stylesheet" type="text/css" href="../src/plugins/jquery-steps/jquery.steps.css"
 		/>
@@ -294,42 +295,48 @@
 										<div class="col-md-6">
 											<div class="form-group">
 												<label>Apellidos:</label>
-												<input type="text" class="form-control" id="per_apellidos" name="per_apellidos"/>
+												<input type="text" class="form-control" id="per_apellidos" name="per_apellidos" required/>
 											</div>
 										</div>
 										
 									</div>
 									<div class="row">
-										<div class="col-md-6">
+										<div class="col-md-4">
 											<div class="form-group">
 												<label>Dirección:</label>
-												<input type="text" class="form-control" id="per_direccion" name="per_direccion"/>
+												<input type="text" class="form-control" id="per_direccion" name="per_direccion" required/>
 											</div>
 										</div>
-										<div class="col-md-6">
+										<div class="col-md-4">
 											<div class="form-group">
 												<label>Colonia:</label>
-												<input type="text" class="form-control" id="per_colonia" name="per_colonia" />
+												<input type="text" class="form-control" id="per_colonia" name="per_colonia" required />
+											</div>
+										</div>
+                                        <div class="col-md-4">
+											<div class="form-group">
+												<label>Municipio:</label>
+												<input type="text" class="form-control" id="per_municipio" name="per_municipio" required/>
 											</div>
 										</div>
 									</div>
                                     <div class="row">
 										<div class="col-md-4">
 											<div class="form-group">
-												<label>Municipio:</label>
-												<input type="text" class="form-control" id="per_municipio" name="per_municipio" />
+												<label>Fecha de nacimiento</label>
+												<input type="date" class="form-control  id="per_fechaNac" name="per_fechaNac" required/>
 											</div>
 										</div>
 										<div class="col-md-4">
 											<div class="form-group">
 												<label>Teléfono:</label>
-												<input type="text" class="form-control" id="per_telefono" name="per_telefono" />
+												<input type="text" class="form-control" id="per_telefono" name="per_telefono" required/>
 											</div>
 										</div>
                                         <div class="col-md-4">
 											<div class="form-group">
 												<label>Teléfono de emergencia:</label>
-												<input type="text" class="form-control" id="per_emergencia" name="per_emergencia"/>
+												<input type="text" class="form-control" id="per_emergencia" name="per_emergencia" required/>
 											</div>
 										</div>
 									</div>
@@ -339,8 +346,8 @@
 												<label>Sexo:</label>
 												<select class="custom-select form-control" id="per_sexo" name="per_sexo">
 													<option selected disabled value="">Seleccionar</option>
-													<option value="Soltero">Femenino</option>
-													<option value="Casado">Masculino</option>
+													<option value="Femenino">Femenino</option>
+													<option value="Masculino">Masculino</option>
 												</select>
 											</div>
 										</div>
@@ -380,10 +387,20 @@
 												<label>Sucursal:</label>
 												<select class="custom-select form-control" id="per_sucursal" name="per_sucursal">
 													<option selected disabled value="">Seleccionar</option>
-													<option value="Ninguna">Ninguna</option>
-													<option value="Sucursal 1">Sucursal 1</option>
-													<option value="Sucursal 2">Secursal 2</option>
-													<option value="Sucursal 3">Sucursal 3</option>
+													<?php
+                                                     $conect = mysqli_connect("localhost"," ","","tablered_bd");
+                                                     $conect->set_charset("utf8");
+                                                     $qry_planes="SELECT * FROM sucursales";
+		                                             if ($resultado = mysqli_query($conect, $qry_planes)) {
+		                                             /* obtener array asociativo */
+		                                             while ($row = mysqli_fetch_assoc($resultado)) {
+		                                                 echo '<option value="'.$row["ID"].'">'.$row["SUCURSAL"].'</option>';
+		                                             }
+		                                             /* liberar el conjunto de resultados */
+		                                             mysqli_free_result($resultado);
+		                                             }
+                                                    echo "<br>";
+    	                                            ?>
 												</select>
 											</div>
 										</div>
@@ -393,7 +410,7 @@
 												<select class="custom-select form-control" id="per_puesto" name="per_puesto">
 													<option selected disabled value="">Seleccionar</option>
 													<?php
-                                                     $conect = mysqli_connect("localhost","root","","tablered_bd");
+                                                     $conect = mysqli_connect("localhost","root","platas123","tablered_bd");
                                                      $conect->set_charset("utf8");
                                                      $qry_planes="SELECT * FROM puestos";
 		                                             if ($resultado = mysqli_query($conect, $qry_planes)) {
@@ -412,7 +429,7 @@
                                         <div class="col-md-4">
 											<div class="form-group">
 												<label>Fecha de alta :</label>
-												<input type="text" class="form-control date-picker" placeholder="Select Date" id="per_fechaalta" name="per_fechaalta"/>
+												<input type="date" class="form-control" placeholder="Select Date" id="per_fechaalta" name="per_fechaalta" required/>
 											</div>
 										</div>
 			
@@ -433,8 +450,8 @@
 												<label>Tipo:</label>
 												<select class="custom-select form-control" id="per_tipo" name="per_tipo">
 													<option selected disabled value="">Seleccionar</option>
-													<option value="Tipo1">Tipo 1</option>
-													<option value="tipo2">Tipo 2</option>
+													<option value="Empleado">Empleado</option>
+													<option value="tipo2">Camionista</option>
 												</select>
 											</div>
 										</div>
@@ -495,33 +512,83 @@
 								<h5>Archivos</h5>
 								<section>
 									<div class="row">
-										<div class="col-md-6">
+										<div class="col-md-12">
                                                 <div class="form-group">
                                                     <label>Correo:</label>
-                                                    <input type="email" class="form-control" id="per_correo" name="per_correo " required/>
+                                                    <input type="email" class="form-control" id="per_correo" name="per_correo" required/>
                                                 </div>
 										</div>
-										<div class="col-md-6">
-                                            <div class="form-group">
-                                                <label>Subir foto</label>
-                                                <input type="file" class="form-control-file form-control height-auto">
-                                            </div>
-										</div>
+										
 									</div>
                                     <div class="row">
-										<div class="col-md-6">
+                                    <div class="col-md-4">
                                             <div class="form-group">
-                                                <label>Subir ID anverso</label>
-                                                <input type="file" class="form-control-file form-control height-auto">
+                                                <label>Subir foto</label>
+                                                <!--<input type="file" name="per_foto" id="per_foto"  class="form-control-file form-control height-auto">-->
+                                                    <div class="photo">
+                                                        <div class="prevPhoto" title="Seleccione una imagen dando click">
+                                                            <span class="delPhoto notBlock" id="X">X</span>
+                                                            <label for="foto"></label>
+                                                        </div>
+                                                        <div class="upimg">
+                                                            <input type="file" name="foto" id="foto">
+                                                        </div>
+                                                        <div id="form_alert"></div>
+                                                    </div>
+
                                             </div>
 										</div>
-										<div class="col-md-6">
+										<div class="col-md-4">
+                                            <div class="form-group">
+                                                <label>Subir ID anverso</label>
+                                                <!--<input type="file" name="per_anverso" id="per_anverso" class="form-control-file form-control height-auto">-->
+                                                <div class="photo2">
+                                                    <div class="prevPhoto2" title="Seleccione una imagen dando click">
+                                                        <span class="delPhoto2 notBlock2" id="X2">X</span>
+                                                        <label for="fotoAnverso"></label>
+                                                    </div>
+                                                    <div class="upimg2">
+                                                        <input type="file" name="fotoAnverso" id="fotoAnverso">
+                                                    </div>
+                                                    <div id="form_alert2"></div>
+                                                </div>
+                                            </div>
+										</div>
+										<div class="col-md-4">
                                             <div class="form-group">
                                                 <label>Subir ID reverso</label>
-                                                <input type="file" class="form-control-file form-control height-auto">
+                                                <!--<input type="file" name="per_reverso" id="per_reverso" class="form-control-file form-control height-auto">-->
+                                                <div class="photo3">
+                                                    <div class="prevPhoto3" title="Seleccione una imagen dando click">
+                                                        <span class="delPhoto3 notBlock3" id="X3">X</span>
+                                                        <label for="fotoReverso"></label>
+                                                    </div>
+                                                    <div class="upimg3">
+                                                        <input type="file" name="fotoReverso" id="fotoReverso">
+                                                    </div>
+                                                    <div id="form_alert3"></div>
+                                                </div>
                                             </div>
 										</div>
 									</div>
+                                    <!-- <div class="col-md-4">
+                                            <div class="">
+                                                <div class="">
+                                                    <div class="photo">
+                                                        <label for="foto" class="text-condensedLight" style="font-size: 18px;">Foto del producto:</label>
+                                                        <p></p>
+                                                        <div class="prevPhoto" title="Seleccione una imagen dando click">
+                                                            <span class="delPhoto notBlock" id="X">X</span>
+                                                            <label for="foto"></label>
+                                                        </div>
+                                                        <div class="upimg">
+                                                            <input type="file" name="foto" id="foto">
+                                                        </div>
+                                                        <div id="form_alert"></div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div> -->
 								</section>
 							</form>
 						</div>
